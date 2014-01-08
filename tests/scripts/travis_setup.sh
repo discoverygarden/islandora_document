5c96d6cf04cb2ca1a376cdd6f14c97fe9fd882be
Nigel Banks <nigel.g.banks@gmail.com>
2014-01-08 16:21:51 -0400

    maybe?

--- tests/scripts/travis_setup.sh
+++ tests/scripts/travis_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd $HOME
-sudo add-apt-repository ppa:upubuntu-com/office
-sudo apt-get update
-sudo apt-get install openoffice
-sudo apt-get install openoffice.org-writer
-sudo apt-get install openoffice.org-draw
-sudo apt-get install openoffice.org-calc
-sudo apt-get install openoffice.org-impress
+sudo add-apt-repository -y ppa:upubuntu-com/office
+sudo apt-get update -y
+sudo apt-get install -y openoffice
+sudo apt-get install -y openoffice.org-writer
+sudo apt-get install -y openoffice.org-draw
+sudo apt-get install -y openoffice.org-calc
+sudo apt-get install -y openoffice.org-impress
 
 soffice -headless -accept="socket,host=127.0.0.1,port=8100;urp;" -nofirststartwizard
 wget http://downloads.sourceforge.net/project/jodconverter/JODConverter/2.2.2/jodconverter-2.2.2.zip
