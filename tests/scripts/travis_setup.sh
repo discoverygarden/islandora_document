e0dbcbb91ed5c41d60008151ef264053b7c6fcb3
lutaylor <luke@discoverygarden.ca>
2014-08-25 11:12:24 -0300

    making travis setup more reliable

--- tests/scripts/travis_setup.sh
+++ tests/scripts/travis_setup.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 cd $HOME
-sudo add-apt-repository -y ppa:upubuntu-com/office
 sudo apt-get update -y
-sudo apt-get install -y openoffice
-sudo apt-get install -y openoffice.org-writer
-sudo apt-get install -y openoffice.org-draw
-sudo apt-get install -y openoffice.org-calc
-sudo apt-get install -y openoffice.org-impress
-
+sudo apt-get install -y libreoffice
+sudo apt-get install -y libreoffice-writer
+sudo apt-get install -y libreoffice-calc
+sudo apt-get install -y libreoffice-impress
+sudo apt-get install -y libreoffice-draw
 echo 'Starting Open Office'
-soffice -headless -accept="socket,host=127.0.0.1,port=8100;urp;" -nofirststartwizard 2>1 &
+/bin/su - www-data -c "/usr/bin/soffice --headless --nologo --nofirststartwizard --accept=\"socket,host=127.0.0.1,port=8100;urp\"" & > /dev/null 2>&1
 echo 'Open Office Started'
-wget http://downloads.sourceforge.net/project/jodconverter/JODConverter/2.2.2/jodconverter-2.2.2.zip
+wget http://alpha.library.yorku.ca/jodconverter-2.2.2.zip
 unzip jodconverter-2.2.2.zip
 cp -r jodconverter-2.2.2 $HOME/drupal-*/sites/all/libraries
 
